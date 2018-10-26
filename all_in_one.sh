7e358997f88493d1946d811093a463422a9a3e63
ivan-garcia-santmaria <ivan.garcias@masmovil.com>
2018-10-26 14:37:08 +0200

    version inicial

---
+++ all_in_one.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+	echo "se necesita el nombre para el uso de los objetos"
+	exit 1
+fi
+
+IFS=
+objetos="$(cat <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: $1
+  name: $1
+spec:
+  ports:
+  - port: 8080
+    protocol: TCP
+    targetPort: 8080
+  selector:
+    app: $1
+  type: ClusterIP
+---
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: $1
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: $1
+  template:
+    metadata:
+      labels:
+        app: $1
+    spec:
+      containers:
+      - image: gcr.io/tranformacion-it-lab/$1:1.0.0
+        imagePullPolicy: Always
+        name: $1
+        ports:
+        - containerPort: 8080
+          protocol: TCP
+EOF
+)"
+
+echo $objetos
---
+++ doImageGen.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+    echo "se necesita el nombre para el uso de los objetos"
+    exit 1
+fi
+
+nombre=$1
+
+mvn clean package -DskipTests
+
+tagGC=`docker images | grep $nombre |awk -v nombre=$nombre -F" " '{if ($1=="gcr.io/tranformacion-it-lab/"nombre) {print $3}}'`
+if [ "$tagGC" != "" ]
+then
+	echo "Borrando la imagen anterior "$tagGC
+	docker rmi $tagGC -f
+fi
+
+echo "Creando la imagen...."
+docker build -t $nombre .
+
+tag=`docker images | grep $nombre |awk -v nombre=$nombre -F" " '{if ($1==nombre) {print $3}}'`
+echo "Creando tag imagen: "$tag
+docker tag $tag gcr.io/tranformacion-it-lab/$nombre:1.0.0
+
+echo "Subiendo a Google Cloud..."
+docker push gcr.io/tranformacion-it-lab/$nombre:1.0.0
+
+pod=`kubectl get pod | grep $nombre | awk -F" " '{print $1}'`
+if [ "$pod" != "" ]
+then
+	echo "Borrando el POD "$pod
+	kubectl delete pod $pod
+else
+	#echo "Creamos deployment"
+	#kubectl run $nombre --replicas=1 --labels="app=$nombre" --image=gcr.io/tranformacion-it-lab/$nombre:1.0.0 --port=8080
+	#echo "Creamos el service"
+	#kubectl expose deployment $nombre --target-port=8080 --type=ClusterIP --name=$nombre
+	echo "Creamos el all in one"
+	DIR="$(cd "$(dirname "$0")" && pwd)"
+        . $DIR/all_in_one.sh $nombre > /tmp/k8_temp.yaml	
+	echo "Creamos los objetos en kubernetes"
+	kubectl create -f /tmp/k8_temp.yaml
+
+fi
+watch kubectl get pod
+
+pod=`kubectl get pod | grep $nombre | awk -F" " '{print $1}'`
+
+kubectl logs -f $pod
