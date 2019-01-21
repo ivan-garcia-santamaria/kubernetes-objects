6c1b79212bbd64fa8903203127e416eb3500fd52
ivan-garcia-santmaria <ivan.garcias@masmovil.com>
2019-01-21 23:19:54 +0100

    ahi

--- infinito.sh
+++ infinito.sh
@@ -20,6 +20,11 @@ while IFS='' read -r linea || [[ -n "$linea" ]]; do
 	tiempo="OJO QUE NO EXISTE POLICY"
    fi
    endpoint=`grep -n $linea apiproxy/proxies/*`
+   ficheroEndpoint=`grep -l $linea apiproxy/proxies/*`
+   if [ "$ficheroEndpoint" != "" ]
+   then
+   	basePath=`grep BasePath $ficheroEndpoint* | sort | awk -F"/" '{print $2"/"$3"/"$4}' | awk -F"<" '{print $1}'`
+   fi
    printf ">%s waitForComplete(): %s<\n" "$linea" "$tiempo"
-   printf "%s;%s;%s\n" "$linea" "$tiempo" "$endpoint"  >> $ficheroWaitForCompleteLimits
+   printf "%s;%s;%s;%s\n" "$linea" "$tiempo" "$basePath" "$endpoint"  >> $ficheroWaitForCompleteLimits
 done < $ficheroWaitForComplete
