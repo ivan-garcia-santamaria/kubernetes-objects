359188c132486a4a5157c6654e23843998277129
ivan-garcia-santmaria <ivan.garcias@masmovil.com>
2019-01-21 21:04:06 +0100

    busqueda en repositorio

--- infinito.sh
+++ infinito.sh
@@ -1,6 +1,8 @@
 
 
-grep -R waitForComplete\(\) | awk -F":" '{print $1}' | sort | uniq | awk -F"/" '{print $4}' | awk -F"." '{print $1}' > ../waitForComplete.txt
+ficheroWaitForComplete="/tmp/waitForComplete.txt"
+
+grep -R waitForComplete\(\) | awk -F":" '{print $1}' | sort | uniq | awk -F"/" '{print $4}' | awk -F"." '{print $1}' > $ficheroWaitForComplete
 
 while IFS='' read -r linea || [[ -n "$linea" ]]; do
    fichero="apiproxy/policies/$linea.xml"
@@ -11,4 +13,4 @@ while IFS='' read -r linea || [[ -n "$linea" ]]; do
 	tiempo="OJO QUE NO EXISTE POLICY"
    fi
    printf ">%s waitForComplete(): %s<\n" "$linea" "$tiempo"
-done < ../waitForComplete.txt
+done < $ficheroWaitForComplete
